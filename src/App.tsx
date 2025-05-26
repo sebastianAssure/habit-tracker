6739416843e6904272021361e0f256074d3ade65
Sebastian Padilla <sebastianpadilla@ascbomacmini80.local>
2025-05-26 15:20:38 -0400

    feat: add base functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-import './App.css'
-import { HabitCard } from './components/HabitCard'
-import { Header } from './components/Header'
-import { NewHabitForm } from './components/NewHabitForm'
+import { useState } from "react";
+import "./App.css";
+import { HabitCard } from "./components/HabitCard";
+import { Header } from "./components/Header";
+import { NewHabitForm } from "./components/NewHabitForm";
+import type { Habit } from "./interfaces/types";
 
 function App() {
+  const [habits, setHabits] = useState<Habit[]>([]);
+
+  const handleAddHabit = (habit: Habit) => {
+    console.log(habit);
+    setHabits((prev) => [...prev, habit]);
+  };
 
   return (
-    <main className='flex flex-col items-center min-h-screen'>
-      <Header></Header>
-      <NewHabitForm></NewHabitForm>
-      <HabitCard></HabitCard>
+    <main className="flex flex-col items-center min-h-screen">
+      <Header/>
+      <NewHabitForm onAddHabit={handleAddHabit} />
+      <HabitCard/>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
